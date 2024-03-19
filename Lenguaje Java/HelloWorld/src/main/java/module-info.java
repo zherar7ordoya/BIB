a07ee3ef9b93ec89252867d21e28dbbbb5d4f2d7
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-03-19 12:54:30 -0300

    2024-03-19

--- Lenguaje Java/HelloWorld/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.helloworld {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.helloworld to javafx.fxml;
-    exports org.example.helloworld;
-}
