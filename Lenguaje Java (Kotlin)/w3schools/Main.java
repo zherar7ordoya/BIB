d82b6ab64090a1f49d48900e9f323dfe8f026211
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-03-25 12:53:29 -0300

    Create Main.java

---
+++ Lenguaje Java (Kotlin)/w3schools/Main.java
@@ -0,0 +1,20 @@
+/*
+ * ¿Qué es esto?
+    Un comentario en Java
+ */
+
+
+ // Para compilar: javac Main.java
+public class Main {
+    // "final" es igual que "readonly" en C#
+    final String Nombre = "Juan";
+    int Edad = 20;
+    double Salario = 1000.50;
+    boolean EsEstudiante = true;
+    char Sexo = 'M';
+
+    public static void main(String[] args) {
+      System.out.println("Hello {Nombre}");
+    }
+  }
+  
