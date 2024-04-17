cab661b80ed3423fc5f6432ccbb0f905c02090d6
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-04-17 19:40:39 -0300

    2024-04-17

--- # GoF/(MVC)/ChatGPT/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.zherar7ordoya.chatgpt {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.zherar7ordoya.chatgpt to javafx.fxml;
-    exports com.zherar7ordoya.chatgpt;
-}
