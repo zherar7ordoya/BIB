12981fc3235e4317fac73ac83790a4b42d9a9f48
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-05-24 19:47:44 -0300

    Update Cliente.cs

--- # GoF/# Freeman & Robson - Head First Design Patterns (2nd edition)/Delegados y Eventos/En C#/#Validador/Cliente.cs
+++ # GoF/# Freeman & Robson - Head First Design Patterns (2nd edition)/Delegados y Eventos/En C#/#Validador/Cliente.cs
@@ -1,4 +1,68 @@
-﻿using System;
+﻿//using System;
+//using System.Xml.Serialization;
+
+//namespace Validador
+//{
+//    [Serializable]
+//    public class Cliente
+//    {
+//        // Eventos para feedback de validación
+//        public event EventHandler<string> ValidacionFallida;
+
+//        // Propiedades con validación
+//        private string _nombre;
+
+//        [XmlElement]
+//        public string Nombre
+//        {
+//            get => _nombre;
+//            set
+//            {
+//                if (!Validaciones.ValidarNombre(value, OnValidacionFallida))
+//                    throw new ArgumentException("Nombre no es válido.");
+//                _nombre = value;
+//            }
+//        }
+
+//        private string _email;
+
+//        [XmlElement]
+//        public string Email
+//        {
+//            get => _email;
+//            set
+//            {
+//                if (!Validaciones.ValidarEmail(value, OnValidacionFallida))
+//                    throw new ArgumentException("Email no es válido.");
+//                _email = value;
+//            }
+//        }
+
+//        // Constructor público para serialización (verificar si esto es así)
+//        public Cliente() { }
+
+//        // Constructor personalizado
+//        public Cliente(string nombre, string email)
+//        {
+//            ValidacionFallida += Cliente_ValidacionFallida;
+
+//            Nombre = nombre;
+//            Email = email;
+//        }
+
+//        private void Cliente_ValidacionFallida(object sender, string e)
+//        {
+//            throw new ArgumentException(e);
+//        }
+
+//        private void OnValidacionFallida(string mensajeError)
+//        {
+//            ValidacionFallida?.Invoke(this, mensajeError);
+//        }
+//    }
+//}
+
+using System;
 using System.Xml.Serialization;
 
 namespace Validador
@@ -6,55 +70,49 @@ namespace Validador
     [Serializable]
     public class Cliente
     {
-        // Eventos para feedback de validación
+        // Evento para notificar de validaciones fallidas
         public event EventHandler<string> ValidacionFallida;
 
         // Propiedades con validación
-        private string _nombre;
+        [XmlElement]
+        public string Nombre { get; private set; }
 
         [XmlElement]
-        public string Nombre
-        {
-            get => _nombre;
-            set
-            {
-                if (!Validaciones.ValidarNombre(value, OnValidacionFallida))
-                    throw new ArgumentException("Nombre no es válido.");
-                _nombre = value;
-            }
-        }
+        public string Email { get; private set; }
 
-        private string _email;
-
-        [XmlElement]
-        public string Email
-        {
-            get => _email;
-            set
-            {
-                if (!Validaciones.ValidarEmail(value, OnValidacionFallida))
-                    throw new ArgumentException("Email no es válido.");
-                _email = value;
-            }
-        }
-
-        // Constructor público para serialización (verificar si esto es así)
+        // Constructor público para serialización
         public Cliente() { }
 
         // Constructor personalizado
         public Cliente(string nombre, string email)
         {
-            ValidacionFallida += Cliente_ValidacionFallida;
+            ValidarYAsignarNombre(nombre);
+            ValidarYAsignarEmail(email);
+        }
 
+        // Método privado para validar y asignar el nombre
+        private void ValidarYAsignarNombre(string nombre)
+        {
+            if (!Validaciones.ValidarNombre(nombre, OnValidacionFallida))
+            {
+                //OnValidacionFallida("Nombre no es válido.");
+                throw new ArgumentException("Nombre no es válido.");
+            }
             Nombre = nombre;
+        }
+
+        // Método privado para validar y asignar el email
+        private void ValidarYAsignarEmail(string email)
+        {
+            if (!Validaciones.ValidarEmail(email, OnValidacionFallida))
+            {
+                //OnValidacionFallida("Email no es válido.");
+                throw new ArgumentException("Email no es válido.");
+            }
             Email = email;
         }
 
-        private void Cliente_ValidacionFallida(object sender, string e)
-        {
-            throw new ArgumentException(e);
-        }
-
+        // Método para invocar el evento de validación fallida
         private void OnValidacionFallida(string mensajeError)
         {
             ValidacionFallida?.Invoke(this, mensajeError);
