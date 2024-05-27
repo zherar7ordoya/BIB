cbe5bdf4f107d142eda42639f1792626f677acfb
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-05-27 09:17:56 -0300

    2024-05-27

---
+++ # GoF/# Freeman & Robson - Head First Design Patterns (2nd edition)/Delegados y Eventos/En C#/Callback (Nicosio)/CRefriSink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap_07_Interfaz_callBack
+{
+    internal class CRefriSink : IEventoRefri    
+    {
+        //metodo del sink
+        public void EReservasBajas(int pKilos)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"==> Reservas bajas de alimentos (quedan {pKilos} kilos)");
+        }
+    }
+}
---
+++ # GoF/# Freeman & Robson - Head First Design Patterns (2nd edition)/Delegados y Eventos/En C#/Callback (Nicosio)/IEventoRefri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap_07_Interfaz_callBack
+{
+    internal interface IEventoRefri
+    {
+        //eventos que tendra refri
+        void EReservasBajas(int pKilos);
+    }
+}
