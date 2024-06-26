08a776a3cb5770f9ce0e9944872070523a74d661
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-06-26 19:48:01 -0300

    2024-06-26

--- C# Designs Patterns/Metsker/OPERATIONS/Command/Stock/Patrones.Command/Program.cs
+++ C# Designs Patterns/Metsker/OPERATIONS/Command/Stock/Patrones.Command/Program.cs
@@ -14,23 +14,20 @@ namespace Patrones.Command.UI
                 Cantidad = 100
             };
 
-            var alta1 = new AltaStockCommand(producto, 10);
-            empresa.TomarOrden(alta1);
+            OrdenCommand comando;
 
-            var baja1 = new BajaStockCommand(producto, 50);
-            empresa.TomarOrden(baja1);
-
-            var baja2 = new BajaStockCommand(producto, 5);
-            empresa.TomarOrden(baja2);
-
-            var baja3 = new BajaStockCommand(producto, 15);
-            empresa.TomarOrden(baja3);
-
-            var alta2 = new AltaStockCommand(producto, 100);
-            empresa.TomarOrden(alta2);
-
-            var alta3 = new AltaStockCommand(producto, 100);
-            empresa.TomarOrden(alta3);
+            comando = new AltaStockCommand(producto, 10);
+            empresa.TomarOrden(comando);
+            comando = new BajaStockCommand(producto, 50);
+            empresa.TomarOrden(comando);
+            comando = new BajaStockCommand(producto, 5);
+            empresa.TomarOrden(comando);
+            comando = new BajaStockCommand(producto, 15);
+            empresa.TomarOrden(comando);
+            comando = new AltaStockCommand(producto, 100);
+            empresa.TomarOrden(comando);
+            comando = new AltaStockCommand(producto, 100);
+            empresa.TomarOrden(comando);
 
             empresa.ProcesarOrdenes();
 
