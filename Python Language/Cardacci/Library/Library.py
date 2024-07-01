b38d7cc55cac80fea0c6459c3fa0835b5f56157b
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-07-01 18:33:18 -0300

    2024-07-01

---
+++ Python Language/Cardacci/Library/Library.py
@@ -0,0 +1,28 @@
+# Hacer una clase estática que pueda ser llamada y devuelva unas operaciones
+# matemáticas con los números 40 y 10.
+
+class Library:
+    @staticmethod
+    def mathOperations():
+        print("40 + 10 =", 40 + 10)
+        print("40 - 10 =", 40 - 10)
+        print("40 * 10 =", 40 * 10)
+        print("40 / 10 =", 40 / 10)
+        print("40 % 10 =", 40 % 10)
+        print("40 // 10 =", 40 // 10)
+        print("40 ** 10 =", 40 ** 10)
+        print("40 ^ 10 =", 40 ^ 10)
+        print("40 & 10 =", 40 & 10)
+        print("40 | 10 =", 40 | 10)
+        print("40 << 10 =", 40 << 10)
+        print("40 >> 10 =", 40 >> 10)
+        print("40 == 10 =", 40 == 10)
+        print("40 != 10 =", 40 != 10)
+        print("40 > 10 =", 40 > 10)
+        print("40 < 10 =", 40 < 10)
+        print("40 >= 10 =", 40 >= 10)
+        print("40 <= 10 =", 40 <= 10)
+        print("40 and 10 =", 40 and 10)
+        print("40 or 10 =", 40 or 10)
+        print("not 40 =", not 40)
+        print("not 10 =", not 10)
---
+++ Python Language/Cardacci/UI/UI.py
@@ -0,0 +1,7 @@
+# Hacer un mensaje de bienvenida. Además, mostrar el resultado de operaciones
+# matemáticas llamando a la clase estática de la librería.
+
+from Library import Library
+
+print("Bienvenido a Python")
+Library.mathOperations()
