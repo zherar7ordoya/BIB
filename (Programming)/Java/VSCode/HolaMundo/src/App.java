6aee8d5a48cd9067852caba079b3d44ab247ae2b
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-07-01 09:16:13 -0300

    2024-07-01

---
+++ (Programming)/Java/VSCode/HolaMundo/src/App.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        System.out.println("Hello, World!");
+    }
+}
