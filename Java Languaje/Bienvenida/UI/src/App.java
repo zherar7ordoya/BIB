0b1080cf5bdfabc8f64261d74cfabde1c1f12fc6
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-07-02 17:38:58 -0300

    2024-07-02

--- Java Languaje/Bienvenida/UI/src/App.java
+++ Java Languaje/Bienvenida/UI/src/App.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        System.out.println("Hello, World!");
+        System.out.println("Bienvenido a Java");
     }
 }
